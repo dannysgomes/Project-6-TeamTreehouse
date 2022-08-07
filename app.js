e6ac6668b94c7a8094fb5faf9a60805dad3c4866
dannysgomes <90053718+dannysgomes@users.noreply.github.com>
2022-08-07 14:55:20 -0400

    Javascript barebones

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-function getElementsByClassName(btn__reset).click(()=> {
-    console.log('it worked');
-})
+const qwerty = document.getElementById("qwerty");
+   
+
+const phrase = document.getElementById("phrase");
+    
+
+const resetBtn = document.getElementsByClassName("btn__reset");
+
+console.log({qwerty, phrase, resetBtn})
+
+let missed = 0;
+
+const phrases = ["Hello Daniel", "Up and Down", "Left and Right", "See you Later", "Good morning"];
+
+const getRandomPhraseAsArray = arr => {
+
+}
+
+const addPhraseToDisplay = arr => {
+
+}
+
+const checkLetter = button => {
+
+}
+
+const checkWin = () => {
+
+}
+
+startButton.addEventListener('click', () => {
+
+});
+
+qwerty.addEventListener('click', e => {
+
+});
